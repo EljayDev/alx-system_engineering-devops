cf0165018834cbce50d8b21ae1609fa9795671ec
EljayDev <eljaydev01@gmail.com>
2023-08-08 03:14:45 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-Query Reddit API to find number of subscribers of a given subreddit
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Args:
-        subreddit (str): Subreddit name to query Reddit API
-
-    Return:
-        subs (int): number of subscribers, 0 if not a valid subreddit
-    """
-    headers = {'User-Agent': 'Holberton Student 329'}
-    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-    sr = requests.get(url, headers=headers)
-    if sr.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    subs = sr.json().get('data').get('subscribers')
-    return subs
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""
-Query Reddit API to find top 10 hot posts of a given subreddit
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of first 10 hot posts for a given subreddit
-
-    Args:
-        subreddit (str): Subreddit name to query Reddit API
-    """
-    headers = {'User-Agent': 'Holberton Student 329'}
-    check_url = 'https://reddit.com/api/search_reddit_names.json'
-    params = {'query': subreddit, 'exact': True}
-    check = requests.get(check_url, headers=headers, params=params)
-    if check.status_code != 200 or len(check.json()['names']) is 0:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-    url = 'https://reddit.com/r/{}/hot/.json'.format(subreddit)
-    sr = requests.get(url, headers=headers)
-    hot_posts = sr.json().get('data').get('children')
-    i = 0
-    while i < len(hot_posts) and i < 10:
-        print(hot_posts[i].get('data').get('title'))
-        i += 1
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
